ec1e796bde4d5e1e7251c0ae44996831b4406ef0
charan819 <charanvss2114@gmail.com>
2025-05-06 19:31:47 +0530

    project apis done

--- mytaskly-backend/src/main/java/com/charan/mytaskly/controllers/ProjectsController.java
+++ mytaskly-backend/src/main/java/com/charan/mytaskly/controllers/ProjectsController.java
@@ -1,5 +1,6 @@
 package com.charan.mytaskly.controllers;
 
+import com.charan.mytaskly.dto.ProjectDto;
 import com.charan.mytaskly.entities.Projects;
 import com.charan.mytaskly.services.ProjectsService;
 import org.springframework.http.ResponseEntity;
@@ -22,16 +23,6 @@ public class ProjectsController {
         return ResponseEntity.ok(projectsService.saveProject(organizationName,projects));
     }
 
-    @GetMapping
-    public ResponseEntity<List<Projects>> getAllProjects(){
-        return ResponseEntity.ok(projectsService.getAllProjects());
-    }
-
-    @GetMapping("/{projectId}")
-    public ResponseEntity<Projects> getProjectByProjectId(@PathVariable("projectId") String projectId){
-        return ResponseEntity.ok(projectsService.getProjectByProjectId(projectId));
-    }
-
     @PutMapping("/{projectId}")
     public ResponseEntity<Projects> updateProjectByProjectId(@PathVariable("projectId") String projectId,@RequestBody Projects projects){
         return ResponseEntity.ok(projectsService.updateProjectByProjectId(projectId,projects));
@@ -40,6 +31,10 @@ public class ProjectsController {
     @DeleteMapping("/{projectId}")
     public ResponseEntity<String> deleteProjectByProjectId(@PathVariable("projectId") String projectId){
         return ResponseEntity.ok(projectsService.deleteProjectByProjectId(projectId));
+    }
+    @GetMapping("/{organizationName}/organizationName")
+    public ResponseEntity<List<ProjectDto>> getProjectByOrganizationName(@PathVariable("organizationName") String organizationName){
+        return ResponseEntity.ok(projectsService.getProjectByOrganizationName(organizationName));
     }
 
 }
--- mytaskly-backend/src/main/java/com/charan/mytaskly/services/ProjectsService.java
+++ mytaskly-backend/src/main/java/com/charan/mytaskly/services/ProjectsService.java
@@ -1,5 +1,6 @@
 package com.charan.mytaskly.services;
 
+import com.charan.mytaskly.dto.ProjectDto;
 import com.charan.mytaskly.entities.Projects;
 
 import java.util.List;
@@ -8,11 +9,10 @@ public interface ProjectsService {
 
     Projects saveProject(String organizationName, Projects projects);
 
-    List<Projects> getAllProjects();
-
-    Projects getProjectByProjectId(String projectId);
-
     Projects updateProjectByProjectId(String projectId, Projects projects);
 
     String deleteProjectByProjectId(String projectId);
+
+    List<ProjectDto> getProjectByOrganizationName(String organizationName);
+
 }
